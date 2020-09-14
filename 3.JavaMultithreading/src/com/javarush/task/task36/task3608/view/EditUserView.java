10eb303dfb310f126121a0a07ec828f943099b74
Ag-o <ag.my.original.@gmail.com>
2020-09-14 10:18:16 +0300

    mvc

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+public class EditUserView implements View {
+
+    private Controller controller;
+
+    public void fireEventUserChanged(String name, long id, int level) {
+        controller.onUserChange(name, id, level);
+    }
+
+    public void fireEventUserDeleted(long id) {
+        controller.onUserDelete(id);
+    }
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
+        //for (User u : modelData.getUsers()) System.out.println("\t" + u);
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
